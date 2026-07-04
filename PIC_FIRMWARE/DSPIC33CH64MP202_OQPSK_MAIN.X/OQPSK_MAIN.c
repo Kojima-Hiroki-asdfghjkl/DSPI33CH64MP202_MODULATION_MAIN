57c83a9d5a6d546befce533c66c8a91921714133
Kojima-Hiroki-asdfghjkl <153900555+Kojima-Hiroki-asdfghjkl@users.noreply.github.com>
2026-07-04 23:05:54 +0900

    Update OQPSK_MAIN.c
    初期設定完了しました．
    一応これで動作しているので，FOSCとか基本的な問題は無いと思う

--- PIC_FIRMWARE/DSPIC33CH64MP202_OQPSK_MAIN.X/OQPSK_MAIN.c
+++ PIC_FIRMWARE/DSPIC33CH64MP202_OQPSK_MAIN.X/OQPSK_MAIN.c
@@ -5,12 +5,46 @@
  * Created on 2026/07/03, 21:00
  */
 
-
+#include "Configuration.h"
 #include <xc.h>
+#include <stdint.h>
 #include "OQPSK_LUT.h"
 
+
+#define FCY 90000000UL//90MHz
+#include <libpic30.h>
+
+
 int main(void) {
+    //WDT SETTING --------------------------------------------------------------
+    WDTCONLbits.ON = 0;//WDT:DISABLE
+    
     //SYSTEM SETTING -----------------------------------------------------------
+    
+    //OSC SETTING --------------------------------------------------------------
+    //MAIN PLL : FOSC:180MHz = FPLL / 2
+    //8MHz * 90 / 2 / 1 = 360MHz
+    //PRIMARY PLL
+    CLKDIV = 0x0001;//PLLPRE 1:1
+    PLLFBD = 90;
+    PLLDIVbits.POST1DIV = 2;
+    PLLDIVbits.POST2DIV = 1;
+    
+    //AUXILIARY PLL : 1000MHz = 8MHz * 125
+    APLLFBD1 = 125;
+    APLLDIV1bits.AVCODIV = 2;//AFVCO / 2
+    
+    
+    //PORT SETTING -------------------------------------------------------------
+    TRISA = 0x0000;
+    ANSELA = 0x0001;//RA0:ANALOG
+    LATA = 0x0002;
+    
+    while (1){
+        LATA ^= 0x0004;
+        __delay_ms(500);
+    }
+    
     
     return 0;
 }
