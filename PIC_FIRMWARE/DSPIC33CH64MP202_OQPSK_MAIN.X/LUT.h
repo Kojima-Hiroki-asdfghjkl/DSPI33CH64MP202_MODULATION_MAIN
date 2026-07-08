262091c8dfa36967f05b64072b22e639278c47e3
Kojima-Hiroki-asdfghjkl <153900555+Kojima-Hiroki-asdfghjkl@users.noreply.github.com>
2026-07-08 20:05:21 +0900

    Rename LUT
    名前を変えたのと，お作法に従って書くようにした

---
+++ PIC_FIRMWARE/DSPIC33CH64MP202_OQPSK_MAIN.X/LUT.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   OQPSK_LUT.h
+ * Author: kojim
+ *
+ * Created on 2026/07/03, 21:02
+ */
+
+#ifndef OQPSK_LUT_H
+#define	OQPSK_LUT_H
+
+#include <stdint.h>
+
+extern const uint16_t SIN_LUT[2][512];
+
+#endif	/* OQPSK_LUT_H */
+
